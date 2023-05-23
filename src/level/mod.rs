75322db773d943a005852a211b1cddd9a7414c9a
Azorlogh <bott.alix@gmail.com>
2023-05-23 13:46:20 +0200

    wip

--- src/level/mod.rs
+++ src/level/mod.rs
@@ -9,9 +9,6 @@ use bevy::{
 	prelude::*,
 	utils::{HashMap, HashSet},
 };
-use bevy_ecs_ldtk::{
-	prelude::*, LdtkPlugin, LdtkSettings, LdtkSystemSet, LevelSelection, LevelSpawnBehavior,
-};
 use bevy_rapier2d::prelude::*;
 
 use crate::states::AppState;
@@ -24,68 +21,46 @@ impl Plugin for LevelPlugin {
 			.add_plugin(launchpad::LaunchpadPlugin)
 			.add_plugin(portal::PortalPlugin)
 			.add_plugin(rope::RopePlugin)
-			.add_plugin(LdtkPlugin)
-			.configure_set(LdtkSystemSet::ProcessApi.before(PhysicsSet::SyncBackend))
-			.insert_resource(LevelSelection::Index(3))
-			.insert_resource(LdtkSettings {
-				level_spawn_behavior: LevelSpawnBehavior::UseZeroTranslation,
-				level_background: LevelBackground::Nonexistent,
-				..default()
-			})
-			.register_ldtk_int_cell::<WallBundle>(1)
-			.register_ldtk_int_cell::<IceBundle>(2)
-			.add_system(spawn_wall_collision::<Wall>)
-			.add_system(spawn_wall_collision::<Ice>)
-			.add_system(bug_workaround)
-			.add_systems(
-				(start::spawn_start, finish::spawn_finish)
-					.distributive_run_if(in_state(AppState::Game)),
-			);
-	}
-}
-
-// There is a bug in `bevy_ecs_ldtk` causing parts of the level to be invisible
-// This seems to fix it
-#[derive(Default)]
-struct LocalWorkaroundState(Vec<(Entity, u32)>);
-fn bug_workaround(
-	mut q_level: Query<&mut Transform>,
-	q_added_level: Query<Entity, Added<Handle<LdtkLevel>>>,
-	mut state: Local<LocalWorkaroundState>,
-) {
-	for e in q_added_level.iter() {
-		state.0.push((e, 60));
-	}
-	for i in (0..state.0.len()).rev() {
-		let (e, t) = &mut state.0[i];
-		*t -= 1;
-		if *t == 0 {
-			if let Ok(mut tr) = q_level.get_mut(*e) {
-				println!("fixed: {e:?}");
-				// tr.translation.x = 0.0001;
-			}
-			state.0.remove(i);
-		}
+			.add_startup_system(load_ldtk_project)
+			.add_system(level_load)
+			// .add_system(spawn_wall_collision::<Wall>)
+			// .add_system(spawn_wall_collision::<Ice>)
+			// .add_systems(
+			// 	(start::spawn_start, finish::spawn_finish)
+			// 		.distributive_run_if(in_state(AppState::Game)),
+			// )
+			;
 	}
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
 pub struct Wall;
 
-#[derive(Clone, Debug, Default, Bundle, LdtkIntCell)]
-pub struct WallBundle {
-	wall: Wall,
-}
-
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
 pub struct Ice;
-#[derive(Clone, Debug, Default, Bundle, LdtkIntCell)]
-pub struct IceBundle {
-	ice: Ice,
-}
 
 #[derive(Component)]
 pub struct RestoresJump;
+
+#[derive(Resource)]
+pub struct LdtkProject(ldtk2::LdtkJsonRoot);
+
+pub fn load_ldtk_project(mut commands: Commands) {
+	let s = std::fs::read_to_string("leaderboard.json").unwrap();
+	let project = ldtk2::LdtkJsonRoot::from_path("assets/levels.ldtk").unwrap();
+	commands.insert_resource(LdtkProject(project));
+}
+
+#[derive(Component)]
+pub struct Level(pub usize);
+
+pub fn level_load(mut commands: Commands, ldtk_project: Res<LdtkProject>, q_level: Query<&Level, Changed<Level>>) {
+	if let Ok(level) = q_level.get_single() {
+		let level = ldtk_project.0.levels.get(level.0).expect("unknown level");
+		let grid_layer = &level.layer_instances.as_ref().unwrap()[0];
+		// dbg!(&grid_layer.identifier);
+	}
+}
 
 pub trait MapTile: Component {
 	fn restores_jump() -> bool {
@@ -101,153 +76,153 @@ impl MapTile for Wall {
 
 impl MapTile for Ice {}
 
-/// Spawn colliders for component T
-pub fn spawn_wall_collision<T: MapTile>(
-	mut commands: Commands,
-	wall_query: Query<(&GridCoords, &Parent), Added<T>>,
-	parent_query: Query<&Parent, Without<T>>,
-	level_query: Query<(Entity, &Handle<LdtkLevel>)>,
-	levels: Res<Assets<LdtkLevel>>,
-) {
-	/// Represents a wide wall that is 1 tile tall
-	/// Used to spawn wall collisions
-	#[derive(Clone, Eq, PartialEq, Debug, Default, Hash)]
-	struct Plate {
-		left: i32,
-		right: i32,
-	}
+// /// Spawn colliders for component T
+// pub fn spawn_wall_collision<T: MapTile>(
+// 	mut commands: Commands,
+// 	wall_query: Query<(&GridCoords, &Parent), Added<T>>,
+// 	parent_query: Query<&Parent, Without<T>>,
+// 	level_query: Query<(Entity, &Handle<LdtkLevel>)>,
+// 	levels: Res<Assets<LdtkLevel>>,
+// ) {
+// 	/// Represents a wide wall that is 1 tile tall
+// 	/// Used to spawn wall collisions
+// 	#[derive(Clone, Eq, PartialEq, Debug, Default, Hash)]
+// 	struct Plate {
+// 		left: i32,
+// 		right: i32,
+// 	}
 
-	/// A simple rectangle type representing a wall of any size
-	struct Rect {
-		left: i32,
-		right: i32,
-		top: i32,
-		bottom: i32,
-	}
+// 	/// A simple rectangle type representing a wall of any size
+// 	struct Rect {
+// 		left: i32,
+// 		right: i32,
+// 		top: i32,
+// 		bottom: i32,
+// 	}
 
-	// Consider where the walls are
-	// storing them as GridCoords in a HashSet for quick, easy lookup
-	//
-	// The key of this map will be the entity of the level the wall belongs to.
-	// This has two consequences in the resulting collision entities:
-	// 1. it forces the walls to be split along level boundaries
-	// 2. it lets us easily add the collision entities as children of the appropriate level entity
-	let mut level_to_wall_locations: HashMap<Entity, HashSet<GridCoords>> = HashMap::new();
+// 	// Consider where the walls are
+// 	// storing them as GridCoords in a HashSet for quick, easy lookup
+// 	//
+// 	// The key of this map will be the entity of the level the wall belongs to.
+// 	// This has two consequences in the resulting collision entities:
+// 	// 1. it forces the walls to be split along level boundaries
+// 	// 2. it lets us easily add the collision entities as children of the appropriate level entity
+// 	let mut level_to_wall_locations: HashMap<Entity, HashSet<GridCoords>> = HashMap::new();
 
-	wall_query.for_each(|(&grid_coords, parent)| {
-		// An intgrid tile's direct parent will be a layer entity, not the level entity
-		// To get the level entity, you need the tile's grandparent.
-		// This is where parent_query comes in.
-		if let Ok(grandparent) = parent_query.get(parent.get()) {
-			level_to_wall_locations
-				.entry(grandparent.get())
-				.or_default()
-				.insert(grid_coords);
-		}
-	});
+// 	wall_query.for_each(|(&grid_coords, parent)| {
+// 		// An intgrid tile's direct parent will be a layer entity, not the level entity
+// 		// To get the level entity, you need the tile's grandparent.
+// 		// This is where parent_query comes in.
+// 		if let Ok(grandparent) = parent_query.get(parent.get()) {
+// 			level_to_wall_locations
+// 				.entry(grandparent.get())
+// 				.or_default()
+// 				.insert(grid_coords);
+// 		}
+// 	});
 
-	if !wall_query.is_empty() {
-		level_query.for_each(|(level_entity, level_handle)| {
-			if let Some(level_walls) = level_to_wall_locations.get(&level_entity) {
-				let level = levels
-					.get(level_handle)
-					.expect("Level should be loaded by this point");
+// 	if !wall_query.is_empty() {
+// 		level_query.for_each(|(level_entity, level_handle)| {
+// 			if let Some(level_walls) = level_to_wall_locations.get(&level_entity) {
+// 				let level = levels
+// 					.get(level_handle)
+// 					.expect("Level should be loaded by this point");
 
-				let LayerInstance {
-					c_wid: width,
-					c_hei: height,
-					grid_size,
-					..
-				} = level
-					.level
-					.layer_instances
-					.clone()
-					.expect("Level asset should have layers")[0];
+// 				let LayerInstance {
+// 					c_wid: width,
+// 					c_hei: height,
+// 					grid_size,
+// 					..
+// 				} = level
+// 					.level
+// 					.layer_instances
+// 					.clone()
+// 					.expect("Level asset should have layers")[0];
 
-				// combine wall tiles into flat "plates" in each individual row
-				let mut plate_stack: Vec<Vec<Plate>> = Vec::new();
+// 				// combine wall tiles into flat "plates" in each individual row
+// 				let mut plate_stack: Vec<Vec<Plate>> = Vec::new();
 
-				for y in 0..height {
-					let mut row_plates: Vec<Plate> = Vec::new();
-					let mut plate_start = None;
+// 				for y in 0..height {
+// 					let mut row_plates: Vec<Plate> = Vec::new();
+// 					let mut plate_start = None;
 
-					// + 1 to the width so the algorithm "terminates" plates that touch the right edge
-					for x in 0..width + 1 {
-						match (plate_start, level_walls.contains(&GridCoords { x, y })) {
-							(Some(s), false) => {
-								row_plates.push(Plate {
-									left: s,
-									right: x - 1,
-								});
-								plate_start = None;
-							}
-							(None, true) => plate_start = Some(x),
-							_ => (),
-						}
-					}
+// 					// + 1 to the width so the algorithm "terminates" plates that touch the right edge
+// 					for x in 0..width + 1 {
+// 						match (plate_start, level_walls.contains(&GridCoords { x, y })) {
+// 							(Some(s), false) => {
+// 								row_plates.push(Plate {
+// 									left: s,
+// 									right: x - 1,
+// 								});
+// 								plate_start = None;
+// 							}
+// 							(None, true) => plate_start = Some(x),
+// 							_ => (),
+// 						}
+// 					}
 
-					plate_stack.push(row_plates);
-				}
+// 					plate_stack.push(row_plates);
+// 				}
 
-				// combine "plates" into rectangles across multiple rows
-				let mut rect_builder: HashMap<Plate, Rect> = HashMap::new();
-				let mut prev_row: Vec<Plate> = Vec::new();
-				let mut wall_rects: Vec<Rect> = Vec::new();
+// 				// combine "plates" into rectangles across multiple rows
+// 				let mut rect_builder: HashMap<Plate, Rect> = HashMap::new();
+// 				let mut prev_row: Vec<Plate> = Vec::new();
+// 				let mut wall_rects: Vec<Rect> = Vec::new();
 
-				// an extra empty row so the algorithm "finishes" the rects that touch the top edge
-				plate_stack.push(Vec::new());
+// 				// an extra empty row so the algorithm "finishes" the rects that touch the top edge
+// 				plate_stack.push(Vec::new());
 
-				for (y, current_row) in plate_stack.into_iter().enumerate() {
-					for prev_plate in &prev_row {
-						if !current_row.contains(prev_plate) {
-							// remove the finished rect so that the same plate in the future starts a new rect
-							if let Some(rect) = rect_builder.remove(prev_plate) {
-								wall_rects.push(rect);
-							}
-						}
-					}
-					for plate in &current_row {
-						rect_builder
-							.entry(plate.clone())
-							.and_modify(|e| e.top += 1)
-							.or_insert(Rect {
-								bottom: y as i32,
-								top: y as i32,
-								left: plate.left,
-								right: plate.right,
-							});
-					}
-					prev_row = current_row;
-				}
+// 				for (y, current_row) in plate_stack.into_iter().enumerate() {
+// 					for prev_plate in &prev_row {
+// 						if !current_row.contains(prev_plate) {
+// 							// remove the finished rect so that the same plate in the future starts a new rect
+// 							if let Some(rect) = rect_builder.remove(prev_plate) {
+// 								wall_rects.push(rect);
+// 							}
+// 						}
+// 					}
+// 					for plate in &current_row {
+// 						rect_builder
+// 							.entry(plate.clone())
+// 							.and_modify(|e| e.top += 1)
+// 							.or_insert(Rect {
+// 								bottom: y as i32,
+// 								top: y as i32,
+// 								left: plate.left,
+// 								right: plate.right,
+// 							});
+// 					}
+// 					prev_row = current_row;
+// 				}
 
-				// commands.entity(level_entity).with_children(|level| {
-					// Spawn colliders for every rectangle..
-					// Making the collider a child of the level serves two purposes:
-					// 1. Adjusts the transforms to be relative to the level for free
-					// 2. the colliders will be despawned automatically when levels unload
-					for wall_rect in wall_rects {
-						// let mut cmds = level.spawn_empty();
-						let mut cmds = commands.spawn(());
-						cmds.insert(Collider::cuboid(
-							(wall_rect.right as f32 - wall_rect.left as f32 + 1.)
-								* grid_size as f32 / 2.,
-							(wall_rect.top as f32 - wall_rect.bottom as f32 + 1.)
-								* grid_size as f32 / 2.,
-						))
-						.insert(RigidBody::Fixed)
-						.insert(Friction::new(1.0))
-						.insert(Transform::from_xyz(
-							(wall_rect.left + wall_rect.right + 1) as f32 * grid_size as f32 / 2.,
-							(wall_rect.bottom + wall_rect.top + 1) as f32 * grid_size as f32 / 2.,
-							0.,
-						))
-						.insert(GlobalTransform::default());
-						if T::restores_jump() {
-							cmds.insert(RestoresJump);
-						}
-					}
-				// });
-			}
-		});
-	}
-}
+// 				// commands.entity(level_entity).with_children(|level| {
+// 					// Spawn colliders for every rectangle..
+// 					// Making the collider a child of the level serves two purposes:
+// 					// 1. Adjusts the transforms to be relative to the level for free
+// 					// 2. the colliders will be despawned automatically when levels unload
+// 					for wall_rect in wall_rects {
+// 						// let mut cmds = level.spawn_empty();
+// 						let mut cmds = commands.spawn(());
+// 						cmds.insert(Collider::cuboid(
+// 							(wall_rect.right as f32 - wall_rect.left as f32 + 1.)
+// 								* grid_size as f32 / 2.,
+// 							(wall_rect.top as f32 - wall_rect.bottom as f32 + 1.)
+// 								* grid_size as f32 / 2.,
+// 						))
+// 						.insert(RigidBody::Fixed)
+// 						.insert(Friction::new(1.0))
+// 						.insert(Transform::from_xyz(
+// 							(wall_rect.left + wall_rect.right + 1) as f32 * grid_size as f32 / 2.,
+// 							(wall_rect.bottom + wall_rect.top + 1) as f32 * grid_size as f32 / 2.,
+// 							0.,
+// 						))
+// 						.insert(GlobalTransform::default());
+// 						if T::restores_jump() {
+// 							cmds.insert(RestoresJump);
+// 						}
+// 					}
+// 				// });
+// 			}
+// 		});
+// 	}
+// }
